cb02eef686f38e22f0df035210e120eaeac276a4
Laura Valana <llaura92@hotmail.com>
2026-04-27 11:59:43 +0200

    init: mise en place layout + styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import StyledComponentsRegistry from "@/lib/registry";
+import {
+  TopBar,
+  NavContainer,
+  IconSymbioseLinkContainer,
+  Researth,
+} from "./layout.styles";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +34,19 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <StyledComponentsRegistry>
+          <TopBar>
+            <NavContainer>
+              <IconSymbioseLinkContainer href="/">
+                {/* <ResearthLogoLayout /> */}
+                <Researth>DJEELING</Researth>
+              </IconSymbioseLinkContainer>
+            </NavContainer>
+          </TopBar>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
